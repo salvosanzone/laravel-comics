29d0324b007a5b0859ec254615efe504b5bbb8c0
salvosanzone <salvosansone.ss@gmail.com>
2022-01-21 15:00:51 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0-9/css/all.min.css" integrity="sha512-X8jR8oYnEmXhGB43u+qxt0mQezR37NUxHhtukvD4bzeWDthOz0mvNOYOzPCMZ7VB0mG607nrkd+kakmSmbffyQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+
+  <header>
+    <div class="container">
+      <div class="logo">
+      <img src="{{asset('img/dc-logo.png')}}" alt="">
+    </div>
+    <div class="menu">
+      <ul>
+        <li>
+          <a href="">links</a>
+        </li>
+        <li>
+          <a href="">links</a>
+        </li>
+        <li>
+          <a href="">links</a>
+        </li>
+        <li>
+          <a href="">links</a>
+        </li>
+        <li>
+          <a href="">links</a>
+        </li>
+        <li>
+          <a href="">links</a>
+        </li>
+        <li>
+          <a href="">links</a>
+        </li>
+        <li>
+          <a href="">links</a>
+        </li>
+        <li>
+          <a href="">links</a>
+        </li>
+        <li>
+          <a href="">links</a>
+        </li>
+      </ul>
+    </div>
+    </div>
+  </header>
+
+  <div class="jumbotron">
+    <div class="top-jumbo">
+      <button class="blue-btn">CURRENT SERIES</button>
+    </div>
+    <div class="bottom-jumbo">
+      <div class="container">
+        <div class="card">
+          <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+          <h6>fumetto</h6>
+        </div>
+        <div class="card">
+          <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+          <h6>fumetto</h6>
+        </div>
+        <div class="card">
+          <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+          <h6>fumetto</h6>
+        </div>
+        <div class="card">
+          <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+          <h6>fumetto</h6>
+        </div>
+        <div class="card">
+          <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+          <h6>fumetto</h6>
+        </div>
+        <div class="card">
+          <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+          <h6>fumetto</h6>
+        </div>
+        <div class="card">
+          <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+          <h6>fumetto</h6>
+        </div>
+        <div class="card">
+          <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+          <h6>fumetto</h6>
+        </div>
+        <div class="card">
+          <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+          <h6>fumetto</h6>
+        </div>
+        <div class="card">
+          <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+          <h6>fumetto</h6>
+        </div>
+        <div class="card">
+          <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+          <h6>fumetto</h6>
+        </div>
+        <div class="card">
+          <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+          <h6>fumetto</h6>
+        </div>
+      </div>
+      <button class="blue-btn">LOAD MORE</button>
+    </div>
+  </div>
+
+  <div class="information">
+    <div class="container">
+      <ul>
+        <li>
+          <div class="img-info">
+            <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt=""> 
+          </div>
+          <a href="">DIGITAL COMICS</a>
+        </li>
+        <li>
+          <div class="img-info">
+            <img src="{{asset('img/buy-comics-merchandise.png')}}" alt=""> 
+          </div>
+          <a href="">dc merchandise</a>
+        </li>
+        <li>
+          <div class="img-info">
+            <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt=""> 
+          </div>
+          <a href="">subscription</a>
+        </li>
+        <li>
+          <div class="img-info">
+            <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt=""> 
+          </div>
+          <a href="">comic shop locator</a>
+        </li>
+        <li>
+          <div class="img-info">
+            <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt=""> 
+          </div>
+          <a href="">dc power visa</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <footer>
+    <div class="top-footer">
+      <div class="container">
+        <div class="link-footer">
+          <ul>
+            <h5>DC COMICS</h5>
+            <li>
+              <a href="">Characters</a>
+            </li>
+            <li>
+              <a href="">Comics</a>
+            </li>
+            <li>
+              <a href="">Movies</a>
+            </li>
+            <li>
+              <a href="">TV</a>
+            </li>
+            <li>
+              <a href="">Games</a>
+            </li>
+            <li>
+              <a href="">Videos</a>
+            </li>
+            <li>
+              <a href="">News</a>
+            </li>
+            <h5>shop dc</h5>
+            <li>
+              <a href="">shop dc</a>
+            </li>
+            <li>
+              <a href="">shop dc collectibles</a>
+            </li>
+          </ul>
+          <ul>
+            <h5>DC</h5>
+            <li>
+              <a href="">Terms of Use</a>
+            </li>
+            <li>
+              <a href="">Privacy Policy (New)</a>
+            </li>
+            <li>
+              <a href="">Ad Choices</a>
+            </li>
+            <li>
+              <a href="">Adversiting</a>
+            </li>
+            <li>
+              <a href="">Jobs</a>
+            </li>
+            <li>
+              <a href="">Subscriptions</a>
+            </li>
+            <li>
+              <a href="">Talent Workshops</a>
+            </li>
+            <li>
+              <a href="">CPSC Certificates</a>
+            </li>
+            <li>
+              <a href="">Ratings</a>
+            </li>
+            <li>
+              <a href="">Shop Help</a>
+            </li>
+            <li>
+              <a href="">Contact Us</a>
+            </li>
+          </ul>
+          <ul>
+            <h5>sites</h5>
+            <li>
+              <a href="">DC</a>
+            </li>
+            <li>
+              <a href="">MAD Magazine</a>
+            </li>
+            <li>
+              <a href="">DC Kids</a>
+            </li>
+            <li>
+              <a href="">DC Universe</a>
+            </li>
+            <li>
+              <a href="">DC Power Visa</a>
+            </li>
+          </ul>
+        </div>
+        <div class="img-footer"></div>
+      </div>
+    </div>
+    <div class="bottom-footer">
+      <div class="container">
+        <div class="shop-now">
+          <div class="btn-shop">
+            <button>
+              <a href="">sign-up now!</a>
+            </button>
+          </div>
+        </div>
+        <div class="socials">
+          <h5>FOLLOW US</h5>
+          <i class="fab fa-facebook"></i>
+          <i class="fab fa-twitter"></i>
+          <i class="fab fa-youtube"></i>
+          <i class="fab fa-pinterest"></i>
+          <i class="fas fa-map-marker-alt"></i>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+</body>
+</html>
